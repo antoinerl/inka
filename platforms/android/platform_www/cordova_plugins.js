669d653874107d79d656b4b466eae402574a90d1
antoine <antoinerdl@gmail.com>
2016-03-09 21:50:17 +0100

    2nd commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "phonegap-plugin-push": "1.5.3",
+    "cordova-plugin-inappbrowser": "1.3.0"
+};
+// BOTTOM OF METADATA
+});
